774ad7b8d537322c2d75e92c035ffde7acaea5e8
Zamarise Montoya <zamarise@gmail.com>
2018-08-15 18:49:18 -0700

    Update app.jsx with title

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -4,6 +4,8 @@ export default class App extends React.Component {
   render() {
     return (
       <div className='container'>
+        <h1>Origin Weather Application</h1>
+        <h2>{'Always know if you\'ll need an umbrella'}</h2>
       </div>
     );
   }
